6e27b92be23a0af807f45f35d40f9a7b7e3ee710
Kai Gu <401146849@qq.com>
2025-01-21 22:45:54 +0800

    add new features and polish documentation

--- R/utils.R
+++ R/utils.R
@@ -163,51 +163,84 @@ h_fmt_count_perc <- function(cnt, perc = NULL, format, ...) {
   num_str
 }
 
-#' @describeIn sfreq Helper Function for counting by groups using [dplyr::count()].
+#' @describeIn sfreq This helper function is designed to generate a summarized data
+#' table with counts and percentages based on specified grouping variables using
+#' [dplyr::count()]. It can also handle hierarchical (nested) variables and apply
+#' various formatting and filtering options.
 #'
-#' @export
+#' @return The `h_count_add_tot` function returns a data frame with counts and
+#' percentages for the specified variables, grouped by the group_by_var. The output
+#' is in a wide format, with additional columns for counts and percentages, and
+#' optionally includes a total column.
 #'
-h_count_by_add_tot <- function(data,
-                               var = NULL,
-                               nested_vars = NULL,
-                               by,
-                               fmt,
-                               tot_df,
-                               dtype,
-                               na_str,
-                               sort = FALSE,
-                               fctdrop = FALSE,
-                               col_tot = TRUE,
-                               nested_row = TRUE) {
-  vars <- c(nested_vars, var, by)
+h_count_add_tot <- function(input_data,
+                            var,
+                            sub_var = NULL,
+                            nested_vars = NULL,
+                            by,
+                            fmt,
+                            distinct_vars = NULL,
+                            cutoff = 0,
+                            tot_df,
+                            dtype,
+                            fctdrop = FALSE,
+                            col_tot = TRUE,
+                            nested_row = TRUE) {
+  # Combine all relevant variables
+  vars <- c(nested_vars, var, sub_var, by)
+  vars_2 <- c(var, nested_vars, sub_var)
   var0 <- c(var, nested_vars)[1]
-  rst1 <- data %>%
-    dplyr::count(!!!syms(vars), sort = sort, .drop = fctdrop) %>%
-    left_join(tot_df, by = by) %>%
-    dplyr::mutate(
-      label := !!sym(var0),
-      dtype = dtype,
-      perc = .data$n / .data$tot
-    ) %>%
-    dplyr::select(label, !!!syms(vars), dtype, dplyr::everything())
 
-  rst2 <- if (col_tot) {
-    rst2 <- data %>%
-      dplyr::count(!!!syms(vars[-length(vars)]), sort = sort, .drop = fctdrop) %>%
-      dplyr::mutate(
-        label := !!sym(var0),
-        dtype = dtype,
-        tot = colSums(tot_df[, "tot"]),
-        perc = .data$n / tot,
-        !!sym(by) := "Total"
-      ) %>%
-      dplyr::select(label, !!!syms(vars), dtype, dplyr::everything())
-  } else {
-    NULL
+
+  # Handle distinct repeated counts
+  dat <- input_data
+  if (!is.null(distinct_vars)) {
+    if (is.null(sub_var)) {
+      dat <- distinct(input_data, !!!syms(c(distinct_vars, var0)), .keep_all = TRUE)
+    } else {
+      dat <- input_data %>%
+        arrange(!!!syms(c(distinct_vars, var0)), desc(!!sym(sub_var))) %>%
+        distinct(!!!syms(c(distinct_vars, var0)), .keep_all = TRUE)
+    }
   }
 
+  # Calculate counts and percentages
+  rst1 <- left_join(
+    dat %>% count(!!!syms(vars_2), .drop = fctdrop) %>% .[, vars_2],
+    dat %>% count(!!!syms(vars), .drop = FALSE),
+    by = vars_2,
+    relationship = "many-to-many"
+  ) %>%
+    left_join(tot_df, by = by) %>%
+    mutate(
+      label := !!sym(var0),
+      # Remove the factor levels from tot_df in case of error occurs in sort process.
+      !!sym(by) := fct_drop(!!sym(by)),
+      dtype = dtype,
+      # special case, only occur in shiny calculation.
+      perc = dplyr::coalesce(.data$n / .data$tot, 0),
+    ) %>%
+    select(label, !!!syms(vars), dtype, everything())
+
+  # Add total column if required
+  rst2 <- NULL
+  if (col_tot) {
+    rst2 <- dat %>%
+      count(!!!syms(vars_2), .drop = fctdrop) %>%
+      mutate(
+        label := !!sym(var0),
+        dtype = dtype,
+        tot = sum(tot_df$tot),
+        perc = dplyr::coalesce(.data$n / .data$tot, 0),
+        !!sym(by) := "Total"
+      ) %>%
+      select(label, !!!syms(vars), dtype, everything())
+  }
+
+  # Combine the results
   df <- rbind(rst1, rst2)
-  
+
+  # Format percentage using `formatters` package
   fmt_lst <- formatters::list_valid_format_labels()
   df$con <- if (fmt %in% fmt_lst$`1d`) {
     h_fmt_count_perc(df$n, format = fmt)
@@ -216,51 +249,98 @@ h_count_by_add_tot <- function(data,
   } else {
     NA
   }
-  list_levels <- split(1:nlevels(df[[by]]), levels(df[[by]]))
-  df$level_n <- sapply(df[[by]], function(x) {
-    list_levels[[x]]
-  })
-  
+
+  # Apply cutoff filter if required
+  df <- if (length(cutoff) == 1) {
+    group_by(df, !!sym(var0)) %>%
+      filter(any(perc >= cutoff / 100)) %>%
+      ungroup()
+  } else if (length(cutoff) == 2) {
+    group_by(df, !!sym(var0)) %>%
+      filter(any(between(perc, cutoff[1] / 100, cutoff[2] / 100))) %>%
+      ungroup()
+  }
+
+  # Add level number for sortings
+  levels_df <- data.frame(1:nlevels(df[[by]]), levels(df[[by]]), stringsAsFactors = FALSE)
+  names(levels_df) <- c("level_n", by)
+  df <- left_join(df, levels_df, by = by) %>%
+    arrange(match(!!sym(by), levels(dat[[by]]))) %>%
+    arrange(!!sym(by) == "Total")
+
+  # Pivot the data to wide format
   df %>%
     tidyr::pivot_wider(
-      id_cols = -c(tidyselect::all_of(by)),
+      id_cols = -all_of(by),
       names_from = "level_n",
       names_sep = "",
-      values_from = c("n", "perc", "tot", "con"),
-      values_fill = na_str
+      values_from = c("n", "perc", "tot", "con")
     )
 }
 
-#' @describeIn sfreq Helper Function for sorting the rows.
+#' @describeIn sfreq This helper function is designed to sort a data frame based
+#' on one or two sorting variables. It handles both factor and non-factor variables
+#' and allows for custom ordering expressions. Additionally, it can manage total
+#' rows and nested rows within the sorting process.
 #'
-#' @export
-#'
+#' @return The `h_count_sort` function returns a data frame with an additional
+#' `var_row_ord` indicating the order of the rows based on the specified sorting criteria.
 h_count_sort <- function(data,
                          df,
                          sort_var,
+                         sort_var2 = NULL,
                          row_tot,
                          nested_row = FALSE,
                          .order = NULL) {
-  sorted_res <- if (is.null(.order)) {
-    if (is.factor(data[[sort_var]])) {
-      df %>% arrange(match(.data[[sort_var]], levels(droplevels(data[[sort_var]]))))
+  sorted_res <- if (is.null(sort_var2)) {
+    if (is.null(.order)) {
+      if (length(row_tot) > 0 & !nested_row) {
+        df %>% arrange(
+          match(
+            .data[[sort_var]],
+            c(row_tot, levels(data[[sort_var]])[which(levels(data[[sort_var]]) != row_tot)])
+          ),
+          !!sym(sort_var)
+        )
+      } else if (is.factor(data[[sort_var]])) {
+        df %>% arrange(match(.data[[sort_var]], levels(data[[sort_var]])))
+      } else {
+        df %>% arrange(!!sym(sort_var))
+      }
     } else {
-      df %>% arrange(!!sym(sort_var))
+      df %>% arrange(!!rlang::parse_expr(.order))
     }
   } else {
-    df %>% arrange(!!rlang::parse_expr(.order))
-  }
-  if (length(row_tot) > 0 & !nested_row) {
-    sorted_res <- df %>% dplyr::arrange(
-      match(
-        .data[[sort_var]],
-        c(row_tot, .data[[sort_var]][which(.data[[sort_var]] != row_tot)])
-      ),
-      sort_col
-    )
+    if (is.null(.order)) {
+      if (is.factor(data[[sort_var]])) {
+        df %>% arrange(
+          match(.data[[sort_var]], levels(data[[sort_var]])),
+          !!sym(sort_var2)
+        )
+      } else {
+        df %>% arrange(!!sym(sort_var), !!sym(sort_var2))
+      }
+    } else {
+      ord_var <- str_remove_all(.order, pattern = "desc|\\(|\\)")
+      var_max_df <- df %>%
+        group_by(!!sym(sort_var)) %>%
+        mutate(var_max = max(!!sym(ord_var)))
+      if (str_detect(.order, "desc")) {
+        var_max_df %>%
+          arrange(desc(.data$var_max), !!sym(sort_var2)) %>%
+          select(-"var_max")
+      } else {
+        var_max_df %>%
+          arrange(.data$var_max, !!sym(sort_var2)) %>%
+          select(-"var_max")
+      }
+    }
   }
   
-  sorted_res %>% ungroup() %>%
+  sorted_res %>%
+    ungroup() %>%
+    mutate(!!sym(sort_var) := factor(!!sym(sort_var), levels = unique(.data[[sort_var]]))) %>%
     group_by(!!sym(sort_var)) %>%
-    mutate(var_row_ord = dplyr::cur_group_rows())
+    mutate(var_row_ord = dplyr::cur_group_id()) %>%
+    ungroup()
 }
